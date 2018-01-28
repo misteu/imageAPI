f53bbbc2214489aca6384fc2efdb2b213086740f
Michael Johannes Steudter <m.steudter@campus.tu-berlin.de>
2018-01-28 12:45:58 +0100

    License update

--- imgCropperService.py
+++ imgCropperService.py
@@ -1,7 +1,7 @@
 
 # MIT License
 
-# Copyright (c) 2018 Michael Steudter
+# Copyright (c) 2018 misteu
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
